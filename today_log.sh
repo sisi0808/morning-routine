1e1f0f842594e2d175aec2991d3575ae2843e675
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2025-01-01 08:35:19 +0900

    Update create new file script

--- today_log.sh
+++ today_log.sh
@@ -8,10 +8,6 @@ DAY=$(date +"%d")
 # 曜日を日本語で取得
 WEEKDAY_JP=$(date +"%a" | sed 's/Mon/月/; s/Tue/火/; s/Wed/水/; s/Thu/木/; s/Fri/金/; s/Sat/土/; s/Sun/日/')
 
-# 月と日の先頭の0を除去
-MONTH=$(echo $MONTH | sed 's/^0//')
-DAY=$(echo $DAY | sed 's/^0//')
-
 # 出力ファイル名を生成
 OUTPUT_FILE="./daily_log/${YEAR}年${MONTH}月${DAY}日(${WEEKDAY_JP}).md"
 
